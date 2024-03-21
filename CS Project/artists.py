8ec9fd32b7a619ba68d40cb7eca3c51eafc75962
Obinna84 <68216674+Obinna84@users.noreply.github.com>
2024-03-20 23:22:35 -0400

    Add files via upload

---
+++ CS Project/artists.py
@@ -0,0 +1,404 @@
+global artists_dict
+artists_dict = {
+    "SZA" : {
+        "genres" : ["contemporary-rnb","jazz","soul","contemporary-pop"],
+        "image" : "/static/sza.jpg"
+    },
+    "Tems" : {
+        "genres" : ["afrobeat","contemporary-rnb"],
+        "image" : "/static/tems.jpeg"
+    }, 
+    "Drake" : {
+        "genres" : ["contemporary-rnb","contemporary-pop","soul","2000s-hip-hop"],
+        "image" : "/static/drake.jpeg"
+    }, 
+    "Rod Wave": {
+        "genres" : ["soul","contemporary-rnb","alt-trap","trap-guitar"],
+        "image" : "/static/rod-wave.jpeg"
+    },
+    "J. Cole" : {
+        "genres" : ["soul","introspective","jazz"],
+        "image" : "/static/j-cole.jpeg"
+    },
+    "Brent Faiyaz" : {
+        "genres" : ["contemporary-rnb","soul","jersey-club"],
+        "image" : "/static/brent.jpeg"
+    },
+    "Michael Jackson" : {
+        "genres" : ["classic-pop","soul","jazz","20th-century-rnb"],
+        "image" : "/static/michael-jackson.jpeg"
+    },
+    "Beyonce" : {
+        "genres" : ["classic-pop","soul","jazz","2000s-rnb"],
+        "image" : "/static/beyonce.jpeg"
+    },
+    "Lil Yatchy" : {
+        "genres" : ["alt-trap","contemporary-pop","melodic-trap","introspective"],
+        "image" : "/static/lil-yatchy.jpeg"
+    }, 
+    "BabyTron" : {
+        "genres" : ["alt-trap","introspective"],
+        "image" : "/static/baby-tron.jpeg"
+    }, 
+    "Future" : {
+        "genres" : ["trap"],
+        "image" : "/static/future.jpeg"
+    }, 
+    "21 Savage" : {
+        "genres" : ["trap"],
+        "image" : "/static/21-savage.jpeg"
+    }, 
+    "Kanye West" : {
+        "genres" : ["jazz","soul","introspective","2000s-hip-hop"],
+        "image" : "/static/kanye.jpeg"
+    }, 
+    "Autumn!" : {
+        "genres" : ["pluggnb","alt-trap","trap","psychedelic"],
+        "image" : "/static/autumn.jpeg"
+    },
+    "Summrs" : {
+        "genres" : ["pluggnb","alt-trap","trap","psychedelic"],
+        "image" : "/static/summrs.jpg"
+    }, 
+    "Yeat" : {
+        "genres" : ["alt-trap","trap","psychedelic"],
+        "image" : ""
+    }, 
+    "LUCKI" : {
+        "genres" : ["alt-trap","trap","psychedelic"],
+        "image" : ""
+    }, 
+    "Juice WRLD" : {
+        "genres" : ["alt-trap","melodic-trap","trap","psychedelic"],
+        "image" : ""
+    }, 
+    "Lil Uzi Vert" : {
+        "genres" : ["alt-trap","melodic-trap","trap","psychedelic","contemporary-pop"],
+        "image" : ""
+    }, 
+    "Whitney Houston" : {
+        "genres" : ["soul","classic-pop","20th-century-rnb"],
+        "image" : ""
+    }, 
+    "Alicia Keys" : {
+        "genres" : ["soul","classic-pop","2000s-rnb"],
+        "image" : ""
+    }, 
+    "Mary J Blige" : {
+        "genres" : ["2000s-rnb","2000s-hip-hop","soul","classic-pop"],
+        "image" : ""
+    },
+    "USHER" : {
+        "genres" : ["2000s-rnb","2000s-hip-hop","soul","contemporary-pop"],
+        "image" : ""
+    }, 
+    "Prince" : {
+        "genres" : ["classic-pop","soul","jazz","20th-century-rnb","classic-rock"],
+        "image" : ""
+    }, 
+    "The Weeknd" : {
+        "genres" : ["contemporary-pop","contemporary-rnb"],
+        "image" : ""
+    }, 
+    "Childish Gambino" : {
+        "genres" : ["soul","contemporary-pop","jazz"],
+        "image" : ""
+    }, 
+    "Gunna" : {
+        "genres" : ["trap"],
+        "image" : ""
+    }, 
+    "Young Thug" : {
+        "genres" : ["trap"],
+        "image" : ""
+    },
+    "Tyler, The Creator" : {
+        "genres" : ["trap","alt-trap","contemporary-pop","melodic-trap"],
+        "image" : ""
+    }, 
+    "Lil Tecca" : {
+        "genres" : ["trap","melodic-trap","contemporary-pop"],
+        "image" : ""
+    }, 
+    "Playboi Carti" : {
+        "genres" : ["trap","melodic-trap","opium","alt-trap"],
+        "image" : ""
+    }, 
+    "Ken Carson" : {
+        "genres" : ["trap","alt-trap","opium"],
+        "image" : ""
+    }, 
+    "Destroy Lonely" : {
+        "genres" : ["trap","alt-trap","opium"],
+        "image" : ""
+    }, 
+    "KANKAN" : {
+        "genres" : ["pluggnb","alt-trap","psychedelic"],
+        "image" : ""
+    }, 
+    "Travis Scott" : {
+        "genres" : ["trap","lofi"],
+        "image" : ""
+    }, 
+    "Kendrick Lamar" : {
+        "genres" : ["introspective","soul"],
+        "image" : ""
+    },
+    "JID" : {
+        "genres" : ["trap","soul","introspective"],
+        "image" : ""
+    }, 
+    "Ariana Grande" : {
+        "genres" : ["contemporary-pop","contemporary-rnb"],
+        "image" : ""
+    }, 
+    "Olivia Rodrigo" : {
+        "genres" : ["contemporary-pop"],
+        "image" : ""
+    }, 
+    "PinkPanthress" : {
+        "genres" : ["contemporary-pop","jersey-club"],
+        "image" : ""
+    }, 
+    "Ice Spice" : {
+        "genres" : ["contemporary-pop","drill","jersey-club"],
+        "image" : ""
+    }, 
+    "Wu-Tang Clan" : {
+        "genres" : ["introspective","soul","jazz"],
+        "image" : ""
+    }, 
+    "Nujabes" : {
+        "genres" : ["introspective","soul","jazz","lofi"],
+        "image" : ""
+    }, 
+    "Nas" : {
+        "genres" : ["introspective","soul","jazz"],
+        "image" : ""
+    }, 
+    "MF DOOM" : {
+        "genres" : ["introspective","soul","jazz","2000s-hip-hop"],
+        "image" : ""
+    },
+    "Tupac" : {
+        "genres" : ["introspective","soul"],
+        "image" : ""
+    }, 
+    "N.W.A" : {
+        "genres" : ["metal"],
+        "image" : ""
+    }, 
+    "Ice Cube" : {
+        "genres" : ["introspective","soul","2000s-hip-hop"],
+        "image" : ""
+    }, 
+    "The Notorious B.I.G" : {
+        "genres" : ["introspective","soul"],
+        "image" : ""
+    }, 
+    "Lil Baby" : {
+        "genres" : ["trap"],
+        "image" : ""
+    }, 
+    "Giveon" : {
+        "genres" : ["jazz","soul","contemporary-rnb"],
+        "image" : ""
+    }, 
+    "Tay-K" : {
+        "genres" : ["trap"],
+        "image" : ""
+    }, 
+    "XXXTentacion" : {
+        "genres" : ["metal","classic-rock","grunge","trap-guitar","trap","alt-trap","melodic-trap"],
+        "image" : ""
+    }, 
+    "Jay-Z" : {
+        "genres" : ["introspective","jazz","soul"],
+        "image" : ""
+    }, 
+    "Daniel Caesar" : {
+        "genres" : ["contemporary-rnb","contemporary-pop","soul","classic-rock"],
+        "image" : ""
+    }, 
+    "Boogie Wit Da Hoodie" : {
+        "genres" : ["contemporary-rnb","trap-gutiar","trap"],
+        "image" : ""
+    }, 
+    "Nirvana" : {
+        "genres" : ["grunge"],
+        "image" : ""
+    }, 
+    "Queen" : {
+        "genres" : ["classic-pop","metal","classic-rock"],
+        "image" : ""
+    }, 
+    "Burna Boy" : {
+        "genres" : ["afrobeat","contemporary-rnb"],
+        "image" : ""
+    }, 
+    "Rema" : {
+        "genres" : ["afrobeat","contemporary-rnb","contemporary-pop"],
+        "image" : ""
+    }, 
+    "J Hus" : {
+        "genres" : ["afrobeat","contemporary-rnb"],
+        "image" : ""
+    }, 
+    "Central Cee" : {
+        "genres" : ["drill"],
+        "image" : ""
+    }, 
+    "Dave" : {
+        "genres" : ["drill","introspective","afrobeat"],
+        "image" : ""
+    }, 
+    "Kodak Black" : {
+        "genres" : ["trap"],
+        "image" : ""
+    }, 
+    "Polo G" : {
+        "genres" : ["trap","melodic-trap","trap-guitar"],
+        "image" : ""
+    }, 
+    "Frank Ocean" : {
+        "genres" : ["contemporary-rnb","contemporary-pop","soul"],
+        "image" : ""
+    }, 
+    "keshi" : {
+        "genres" : ["contemporary-rnb","lofi"],
+        "image" : ""
+    }, 
+    "Rihana" : {
+        "genres" : ["contemporary-rnb","classic-pop"],
+        "image" : ""
+    }, 
+    "DEAN" : {
+        "genres" : ["k-pop","contemporary-rnb"],
+        "image" : ""
+    }, 
+    "NewJeans" : {
+        "genres" : ["k-pop","contemporary-pop","jersey-club"],
+        "image" : ""
+    }, 
+    "LE SSERAFIM" : {
+        "genres" : ["k-pop","contemporary-pop"],
+        "image" : ""
+    }, 
+    "TWICE" : {
+        "genres" : ["k-pop","contemporary-pop","contemporary-rnb"],
+        "image" : ""
+    }, 
+    "BTS" : {
+        "genres" : ["k-pop","contemporary-pop"],
+        "image" : ""
+    }, 
+    "Jung Kook" : {
+        "genres" : ["k-pop","contemporary-pop"],
+        "image" : ""
+    }, 
+    "Tee Grizzley" : {
+        "genres" : ["trap","introspective","drill"],
+        "image" : ""
+    }, 
+    "Ms. Lauryn Hill" : {
+        "genres" : ["soul","introspective","20th-century-rnb"],
+        "image" : ""
+    }, 
+    "Cochise" : {
+        "genres" : ["alt-trap","trap","contemporary-pop"],
+        "image" : ""
+    }, 
+    "Sleepy Hallow" : {
+        "genres" : ["trap-guitar","drill","lofi"],
+        "image" : ""
+    }, 
+    "Sheff G" : {
+        "genres" : ["drill"],
+        "image" : ""
+    }, 
+    "Kyle Richh" : {
+        "genres" : ["drill","jersey-club"],
+        "image" : ""
+    }, 
+    "Chief Keef" : {
+        "genres" : ["drill","trap"],
+        "image" : ""
+    }, 
+    "redveil" : {
+        "genres" : ["jazz","soul","introspective","trap"],
+        "image" : ""
+    }, 
+    "Destiny's Child" : {
+        "genres" : [""],
+        "image" : ""
+    }, 
+    "Bandmanrill" : {
+        "genres" : ["jersey-club","drill"],
+        "image" : ""
+    }, 
+    "Fivio Foreign" : {
+        "genres" : ["drill","soul"],
+        "image" : ""
+    }, 
+    "NBA Young Boy" : {
+        "genres" : ["drill","trap"],
+        "image" : ""
+    }, 
+    "Sha Gz" : {
+        "genres" : ["drill","jersey-club"],
+        "image" : ""
+    }, 
+    "Kay Flock" : {
+        "genres" : ["drill"],
+        "image" : ""
+    }, 
+    "2Rare" : {
+        "genres" : ["jersey-club"],
+        "image" : ""
+    }, 
+    "King Von" : {
+        "genres" : ["drill"],
+        "image" : ""
+    }, 
+    "Draft Day" : {
+        "genres" : ["alt-trap","introspective","trap"],
+        "image" : ""
+    }, 
+    "Tyla" : {
+        "genres" : ["contemporary-pop","afrobeat"],
+        "image" : ""
+    }, 
+    "Arya Starr" : {
+        "genres" : ["contemporary-pop","afrobeat","contemporary-rnb"],
+        "image" : ""
+    }, 
+}
+
+class ArtistList:
+    def __init__(self,genres,songs):
+        self.genres = genres
+        self.artists_displayed = {}
+        artists_list = []
+        for key in artists_dict:
+            curr = Artist(key,self.genres)
+            if curr.total_intersections >= 1 and len(artists_list) < songs:
+                artists_list.append(curr)
+        artists_list = sorted(artists_list, key = lambda Artist : Artist.total_intersections)
+        artists_list.reverse()
+        for i in artists_list:
+            name = i.artist
+            self.artists_displayed[name] = artists_dict[name]
+    def getArtists(self):
+        return self.artists_displayed
+        
+
+class Artist:
+    def __init__(self,artist_name,genres):
+        self.artist = artist_name
+        self.genres = genres
+        self.total_intersections = 0
+        for i in artists_dict[self.artist]["genres"]:
+            if i in self.genres:
+                self.total_intersections += 1
+        
+        
